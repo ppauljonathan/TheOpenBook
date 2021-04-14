5b7b7931ea12a9796fcd5b97ff712bed9ef23996
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-14 11:40:06 +0530

    added ejs templating and uses it in app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const app=require('express')();
+const express=require('express');
+const app=express();
 require('dotenv').config();
 
 const routes=require('./routes/routes');
 const e404=require('./controllers/404');
+
+
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
+
+app.set('views','./views');
 
 app.use(routes);
 
 app.use(e404.get404);
 
 app.use((err,req,res,next)=>{
+    console.log(err);
     res.status(500);
     res.send('internal server error');
 })
--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,3 @@
 module.exports.main=(req,res,next)=>{
-    res.send('hello from controllers');
+    res.render('index');
 }
