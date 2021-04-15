eccfc1f0d8063096bd11dbbd82d6cb8f672ee4cb
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-15 22:25:53 +0530

    made nav elements highlight dynamically

--- controllers/errors.js
+++ controllers/errors.js
@@ -8,6 +8,7 @@ module.exports.get404=(req,res,next)=>{
 module.exports.get500=(err,req,res,next)=>{
     res.status(500);
     res.render('500',{
+        title:'Server Error',
         message:err
     })
 }
