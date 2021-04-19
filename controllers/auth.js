2711b9c934132b0157b3a3defa1ca91182e4f84b
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-19 10:56:37 +0530

    added login and signup views

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,18 @@ module.exports.getLogin=(req,res,next)=>{
     })
 }
 
+module.exports.postLogin=(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+}
+
 module.exports.getSignup=(req,res,next)=>{
     res.render('auth/signup',{
         title:'Signup'
     })
+}
+
+module.exports.postSignup=(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/login');
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const router=require('express').Router();
 const authControllers=require('../controllers/auth');
 
 router.get('/login',authControllers.getLogin);
+router.post('/login',authControllers.postLogin);
 
 router.get('/signup',authControllers.getSignup);
+router.post('/signup',authControllers.postSignup);
 
 module.exports=router;
