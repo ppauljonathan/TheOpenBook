fbf37445cf2cfbfe60b8b1f05077a171682334f4
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-20 10:24:16 +0530

    added express-validator and used it

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,40 @@
 const router=require('express').Router();
+const {body}=require('express-validator');
+
+const User=require('../models/user');
+
 const authControllers=require('../controllers/auth');
 
 router.get('/login',authControllers.getLogin);
-router.post('/login',authControllers.postLogin);
+router.post(
+    '/login',
+    authControllers.postLogin
+);
 
 router.get('/signup',authControllers.getSignup);
-router.post('/signup',authControllers.postSignup);
+router.post(
+    '/signup',
+    [
+        body('email')
+        .isEmail()
+        .trim()
+        .withMessage('Please enter a valid email'),
+        body('username')
+        .trim(),
+        body('password')
+        .trim()
+        .isLength({min:5})
+        .withMessage("password too short"),
+        body('confirmPassword')
+        .trim()
+        .custom((val,{req})=>{
+            if(val.toString()!==req.body.password.toString()){
+                return Promise.reject("passwords do not match");
+            }
+            return true;
+        })
+    ],
+    authControllers.postSignup
+);
 
 module.exports=router;
