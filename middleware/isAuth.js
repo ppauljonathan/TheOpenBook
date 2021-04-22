ee11afb55ddabe2540c3286c4b3b1b4dad7e6966
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-22 10:46:12 +0530

    fixed bug in sessions middleware

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,3 +1,5 @@
+const mongoose=require('mongoose');
+
 const Session=require('../models/session');
 
 module.exports.isAuth=(req,res,next)=>{
@@ -28,15 +30,21 @@ module.exports.clearSessions=(req,res,next)=>{
 
 module.exports.assignUser=(req,res,next)=>{
     if(!req.headers.cookie){next();}
+    else if(!mongoose.isValidObjectId(req.headers.cookie.split('=')[1])){
+        res
+        .clearCookie("session")
+        .redirect('/login');
+    }
     else{
         Session
         .findById(req.headers.cookie.split('=')[1])
         .then(session=>{
-            if(!session.user){
-                return res.clearCookie("session");
-            }
-            req.isLoggedIn=true;
-            req.userId=session.user.toString();
+            console.log(session);
+            // if(!session.user){
+            //     return res.clearCookie("session");
+            // }
+            // req.isLoggedIn=true;
+            // req.userId=session.user.toString();
             next();
         })
         .catch(err=>{
