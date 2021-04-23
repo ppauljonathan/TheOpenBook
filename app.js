c43acd45ef99dc415fc6b6dce11bdf84e9dbd733
P Paul Jonathan <ppauljonathan@gmail.com>
2021-04-23 13:24:32 +0530

    used express-session

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express=require('express');
 const app=express();
-require('dotenv').config();
+const dotenv=require('dotenv');
+dotenv.config();
 const mongoose=require('mongoose');
+const session=require('express-session');
+const MongoStore=require('connect-mongodb-session')(session);
+
 
 const routes=require('./routes/routes');
 const authRoutes=require('./routes/auth');
 const errorHandlers=require('./controllers/errors');
 
-const MONGO_URI=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@theopenbook.q3gox.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const MONGO_URI=`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@theopenbook.q3gox.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+const store=new MongoStore({
+    uri:MONGO_URI,
+    collection:'sessions'
+})
+
+app.use(session({
+    secret:'mysupersupersecretsecret',
+    resave:false,
+    cookie:{
+        maxAge:1000*60*45
+    },
+    saveUninitialized:true,
+    store:store
+}))
 
 app.use(express.urlencoded({extended:true}));
 
